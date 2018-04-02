e6b5b5b5691e51ebfc00e28ae2c63f39f7fbe16d
mikemjharris <mike.mj.harris@gmail.com>
2018-04-02 20:28:22 +0100

    get rid of bower.json

--- server/server.js
+++ server/server.js
@@ -16,7 +16,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.static(path.join(__dirname, '../bower_components')));
 
 require('./routes/main')(app);
 
