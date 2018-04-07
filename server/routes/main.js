88508e265a708f4be1d15f1d9c26c5ad258e1df0
mikemjharris <mike.mj.harris@gmail.com>
2018-04-07 18:21:34 +0100

    views to see images

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,10 +1,25 @@
-module.exports = function ( app ) {
 
+const instagramData = require('../data/instagram.json');
+module.exports = function ( app ) {
   
   app.get('/instagram', function( req, res ) {
     const db = req.db;
+    
     db.collection('instagram').find().toArray(function (err, instagram) {
-          res.json({"hello": 1});
+         res.json(instagramData);
     });
+  });
+ 
+
+  app.get('/instagram-images', function( req, res ) {
+    const db = req.db;
+    
+    res.json(instagramData.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media);
+  });
+  
+  app.get('/instagram-app', function( req, res ) {
+    const db = req.db;
+    
+    res.render('images', { "title": "title", "images": instagramData.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges});
   });
 };
