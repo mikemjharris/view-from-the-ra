170fc2531fb0c6e877505585184418adfd3d0cf9
mikemjharris <mike.mj.harris@gmail.com>
2018-04-11 21:36:06 +0100

    sort out scraping script

--- driver.js
+++ driver.js
@@ -1,98 +1,79 @@
 'use strict';
 
-const port = process.env.PORT || 3000;
-const mongoUri = process.env.MONGOLAB_URI || "mongodb://localhost:27017";
-const dbName = 'view-from-the-ra'
+const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017";
 const MongoClient = require('mongodb').MongoClient
+const dbName = 'view-from-the-ra'
+const puppeteer = require('puppeteer');
 
-MongoClient.connect(mongoUri, (err, client) => {
-  if (err) return console.log(err)
-  const db = client.db(dbName)
-
-  const puppeteer = require('puppeteer');
-  const request = require('request');
-  var cookie;
-  var url;
-
-  var initialData;
-
-  function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      body.toString().split(/\n/).forEach((line) => {
-        if (line && /sharedData/.test(line)) {
-          line = line.replace(/.* =/, "")
-          line = line.replace(/;<\/script>/, "")
-          initialData = JSON.parse(line);
-          console.log('Initial data');
-          db.collection('instagram-init').insert(initialData, (err, result) => {
-            if (err) return console.log(err);
-
-          })
-        }
-      })
+function parseHtml(html) {
+  var promise; 
+  html.toString().split(/\n/).forEach((line) => {
+    if (line && /sharedData/.test(line)) {
+      line = line.replace(/.* =/, "");
+      line = line.replace(/;<\/script>/, "")
+      promise = Promise.resolve(JSON.parse(line).entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges);
     }
-  }
+  })
+  return promise;
+}
 
-  //request(options, callback);
+var list_of_promises = [];
 
+(async() => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const timeout = ms => new Promise(res => setTimeout(res, ms))
 
-  console.log('Now running the rest');
+  var url_to_visit = 'https://www.instagram.com/mikemjharris/';
+  page.on('response', response => {
+    var url = response.url().toString();
 
-  (async() => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    
-  function callback(error, response, body) {
-    console.log(response.body);
-    if (!error && response.statusCode == 200) {
-      var remainingImages = JSON.parse(body);
-      return db.collection('instagram-all').insert(remainingImages, (err, result) => {
-        console.log('inserted');
-        if (err) return console.log(err);
-        return result;
-      });
-
-    } else {
-      console.log(error);
+    if (url == url_to_visit) {    
+      console.log('Visiting Page');
+      console.log(response.url());
+      list_of_promises.push(response.text().then((text) => { 
+        return parseHtml(text);
+      }));
     }
-  }
-    console.log('about to call');
-    await page.setRequestInterception(true);
 
-    page.on('request', req => {
-      url = req.url().toString();
-      if (url.search("query_hash") > -1) {    
-        url = req.url().toString();
-      //  console.log(url); 
-      }  
-      req.continue();
-    });
-    
-    page.on('response', response => {
-      var urlw = response.url().toString();
-      if (urlw.search("query_hash") > -1) {    
-          console.log('here');
-          console.log(response.url());
-          response.text().then((text) => { console.log(text);});
-      }
-    })
+    if (url.search("query_hash") > -1 && url.search("first") > -1) {    
+      console.log('Ajax call');
+      console.log(response.url());
+      list_of_promises.push(response.text().then((text) => { 
+        return Promise.resolve(JSON.parse(text).data.user.edge_owner_to_timeline_media.edges);
+      }));
+    }
+  })
 
-    const timeout = ms => new Promise(res => setTimeout(res, ms))
 
-    await page.goto('https://www.instagram.com/mikemjharris');
-    cookie = await page.evaluate(() => document.cookie);
-    console.log('cookie', cookie);
-    console.log('page loaded')
+  await page.goto(url_to_visit);
+  
+  for (var i = 0; i < 7; i++) {
+    await timeout(1000);
     await page.hover('footer');
     await timeout(1000);
     await page.hover('h1');
-    await timeout(1000);
-    console.log('hover footer again');
-    await page.hover('footer');
-    await timeout(5000);
+   }
+  
+  await timeout(5000);
+  
+  console.log('Finished navigating');
 
-    await browser.close();
-    console.log('finished');
-    return
-  })();
-});
+  var res = await Promise.all(list_of_promises);
+  var images = [].concat.apply([], res).map((item) => { return item.node });
+  MongoClient.connect(mongoUri, (err, client) => {
+    if (err) return console.log(err)
+    const db = client.db(dbName)
+    db.collection('images').insert({ 'images': images } , (err, result) => {
+        if (err) return console.log(err);
+        console.log('Images added to db');
+    })
+    client.close();
+  });
+
+  await browser.close();
+
+  console.log('Complete');
+})();
+
+
