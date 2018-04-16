7acb1be41b1582757e4d248a21d7e1deb6c0f51b
mikemjharris <mike.mj.harris@gmail.com>
2018-04-16 21:47:50 +0100

    ignore case when checking tag

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,17 +1,17 @@
 module.exports = ( app ) => {
-  
+
   app.get('/images', ( req, res ) => {
     const db = req.db;
     db.collection('images').find({}).sort({"_id":-1}).limit(1).toArray((err, images) => {
         res.json(images[0].images);
     });
   });
-  
+
   app.get('/', ( req, res ) => {
     const db = req.db;
     db.collection('images').find({}).sort({"_id":-1}).limit(1).toArray((err, images) => {
 			  filteredImages = images[0].images.reduce((arr, image) => {
-					if ( image.edge_media_to_caption.edges.length > 0 && image.edge_media_to_caption.edges[0].node.text.indexOf('ViewFromTheRA') > -1 ) {
+					if ( image.edge_media_to_caption.edges.length > 0 && image.edge_media_to_caption.edges[0].node.text.toLowerCase().indexOf('viewfromthera') > -1 ) {
 						arr.push(image);
 					}
 					return arr
